7dcbee3712519b683303f79d2d7c9cb1ab8bfe78
Ahmed Tokyo <ah02811028@gmail.com>
2023-08-16 03:37:56 -0700

    dd

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,7 +11,6 @@ void times_table(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		_putchar('\n');
 		_putchar('0');
 		for (mlt = 1; mlt <= 9; mlt++)
 		{
@@ -28,6 +27,7 @@ void times_table(void)
 			_putchar((prd % 10) + '0');
 
 		}
+		_putchar('\n');
 	}
 }
 
