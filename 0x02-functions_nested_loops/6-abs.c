19f2fded506151020cf87cb68f6adce5986bc2fe
Ahmed Tokyo <ah02811028@gmail.com>
2023-08-16 01:00:43 -0700

    aa

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @int: number to check
+ *
+ * Return: 0 or 1
+*/
+
+int _abs(int)
+{
+	if (n < 0)
+	{
+		n = (-1) * n;
+		return (0);
+	}
+}
