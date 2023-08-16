3545c2b3b8a990892de969c8cd1379ee7643e677
Ahmed Tokyo <ah02811028@gmail.com>
2023-08-16 02:54:17 -0700

    dd

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * times_table - prints times table
+ * Return:void
+*/
+
+void times_table(void)
+{
+	int num, mlt, prd;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+		for (mlt = 1; mlt <= 9; mlt++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prd = num * mlt;
+
+			if (prd <= 9)
+				_putchar(' ');
+			else
+				_putchar((prd / 10) + '0');
+
+			_putchar((prd % 10) + '0');
+
+		_putchar('\n');
+		}
+	}
+}
+
