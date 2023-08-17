24fa346e6cd7bf28aec057ab91754b78f9bf3f55
Ahmed Tokyo <ah02811028@gmail.com>
2023-08-17 09:06:04 -0700

    ss

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print the numbers from 0 to 9
+ * Return: 0
+*/
+
+void print_numbers(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+		_putchar(n);
+
+	return (0);
+}
+
