3e2739d543ef0972194a450ac419a36034870781
Ahmed Tokyo <ah02811028@gmail.com>
2023-08-17 10:44:23 -0700

    \

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draw a diagonal
+ * @n: number of / to be printed
+ * Return: void
+*/
+
+void print_diagonal(int n)
+{
+	int i = 0;
+	int ii;
+
+	while (n > i && n > 0)
+	{
+		ii = 0;
+		while (i > ii)
+		{
+			_putchar(' ');
+			ii++;
+		}
+
+		_putchar('\\');
+		_putchar('\n');
+		i++;
+	}
+}
