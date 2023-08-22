68ef70e95cabe2afbb939d757d93ff08e4c79308
Ahmed Tokyo <ah02811028@gmail.com>
2023-08-22 03:44:13 -0700

    ss

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,3 @@
-#ifndef MAIN.H
-#define MAIN.H
-
-#include <main.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -15,4 +11,3 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
-#endif
